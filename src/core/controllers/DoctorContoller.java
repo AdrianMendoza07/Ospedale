5e2d3e3135ac0e65be48d5a0419abd4fdcb67e59
Adrian Mendoza <adrian25336@gmail.com>
2026-05-22 00:25:44 -0500

    Update DoctorContoller.java

--- src/core/controllers/DoctorContoller.java
+++ src/core/controllers/DoctorContoller.java
@@ -7,6 +7,7 @@ package core.controllers;
 import core.controllers.utils.Response;
 import core.controllers.utils.Status;
 import core.model.DataRepository;
+import core.model.Specialty;
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 
@@ -15,14 +16,14 @@ import java.time.format.DateTimeParseException;
  * @author adria
  */
 public class DoctorContoller {
-    public static Response createPatient(String id, String username, String firstname, String lastname, String password, String comPassword, String email, String birthdate, String gender, String phone, String address) {
+
+    public static Response createPatient(String id, String username, String firstname, String lastname, String password, String comPassword, String inSpecialty, String license, String office) {
 
         try {
-            long longId, longPhone;
-            boolean boolGender;
-            LocalDate dateBirthdate;
+            long longId;
+            Specialty dSpecialty;
 
-            if(id.trim().equals("")){
+            if (id.trim().equals("")) {
                 return new Response("Id must not be empty", Status.BAD_REQUEST);
             }
             if (username.trim().equals("")) {
@@ -40,14 +41,20 @@ public class DoctorContoller {
             if (comPassword.trim().equals("")) {
                 return new Response("Password comfirmation must not be empty", Status.BAD_REQUEST);
             }
-            if (email.trim().equals("")) {
-                return new Response("Email must not be empty", Status.BAD_REQUEST);
+            if (inSpecialty.trim().equals("Select one")) {
+                return new Response("Must choose an option", Status.BAD_REQUEST);
             }
-            if(phone.trim().equals("")){
-                return new Response("Phone must not be empty", Status.BAD_REQUEST);
+
+            try {
+                dSpecialty = Specialty.valueOf(inSpecialty);
+            } catch (IllegalArgumentException e) {
+                return new Response("Invalid specialty selected", Status.BAD_REQUEST);
             }
-            if (address.trim().equals("")) {
-                return new Response("Address must not be empty", Status.BAD_REQUEST);
+            if (license.trim().equals("")) {
+                return new Response("License must not be empty", Status.BAD_REQUEST);
+            }
+            if (office.trim().equals("")) {
+                return new Response("Office number must not be empty", Status.BAD_REQUEST);
             }
             try {
                 longId = Long.parseLong(id.trim());
@@ -62,45 +69,23 @@ public class DoctorContoller {
                 return new Response("Id must be a number", Status.BAD_REQUEST);
             }
 
-            try {
-                longPhone = Long.parseLong(phone.trim());
-                if (longPhone < 0) {
-                    return new Response("Phone must be positive.", Status.BAD_REQUEST);
-                }
-                if (phone.length() != 12) {
-                    return new Response("Phone must have 12 digits.", Status.BAD_REQUEST);
-                }
-
-            } catch (NumberFormatException e) {
-                return new Response("Phone must be a number", Status.BAD_REQUEST);
+            String licenseRegex = "^L-\\d{10} MTL$";
+            if (!license.trim().matches(licenseRegex)) {
+                return new Response("Not a valid license number", Status.BAD_REQUEST);
             }
 
-            try {
-                dateBirthdate = LocalDate.parse(birthdate);
-            } catch (DateTimeParseException e) {
-                return new Response("Invalid date", Status.BAD_REQUEST);
+            String officeRegex = "^O-\\d{3}$";
+            if (!office.trim().matches(officeRegex)) {
+                return new Response("Not a valid office number", Status.BAD_REQUEST);
             }
 
             if (!password.equals(comPassword)) {
                 return new Response("Password and password comfirmation must be the same", Status.BAD_REQUEST);
             }
 
-            String emailRegex = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.com$";
-            if (!email.matches(emailRegex)) {
-                return new Response("Not a valid email", Status.BAD_REQUEST);
-            }
-
-            if (gender.trim().equals("Female")) {
-                boolGender = false;
-            }
-
-            if (gender.trim().equals("Male")) {
-                boolGender = true;
-            }
-
-            DataRepository storage = DataRepository.getInstace();
-            if (!storage.addPatient(longId, username, firstname, lastname, password, email, dateBirthdate, boolGender, longPhone, address)) {
-                return new Response("Patient with those cretentials already exists", Status.BAD_REQUEST);
+            DataRepository storage = DataRepository.getInstance();
+            if (!storage.addDoctor(longId, username, firstname, lastname, password, license, dSpecialty, office)) {
+                return new Response("Doctor with those cretentials already exists", Status.BAD_REQUEST);
             }
 
             return new Response("Patient created successfully", Status.CREATED);
