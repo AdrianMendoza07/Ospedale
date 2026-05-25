beabbaf3a96fb0e9010bfa8a73a36ff974ef4d6d
Adrian Mendoza <adrian25336@gmail.com>
2026-05-24 23:32:11 -0500

    Update AdministratorController.java

--- src/core/controllers/AdministratorController.java
+++ src/core/controllers/AdministratorController.java
@@ -9,6 +9,7 @@ import core.controllers.utils.Status;
 import core.model.DataRepository;
 import core.model.Doctor;
 import core.model.Patient;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
@@ -27,66 +28,76 @@ public class AdministratorController {
         }
         return new Response("Doctors loaded successfully", Status.OK, doctoresMap);
     }
-    
-    public static Response returnDoctorUsernameToChangeView(String id){
-        
-        try{
+
+    public static Response loadAllPatient() {
+        DataRepository storage = DataRepository.getInstance();
+        ArrayList<Patient> ids = storage.getPatients();
+        HashMap<String, Object> patientIdMap = new HashMap<>();
+
+        for (Patient p : ids) {
+            String id = "" + p.getId();
+            String nombreCompleto = "Dr. " + p.getFirstname() + " " + p.getLastname();
+            patientIdMap.put(id, nombreCompleto);
+        }
+
+        return new Response("Patients loaded sucessfully", Status.OK, patientIdMap);
+
+    }
+
+    public static Response returnDoctorUsernameToChangeView(String id) {
+
+        try {
             long longId;
             try {
-                longId= Long.parseLong(id);
-                
+                longId = Long.parseLong(id);
+
             } catch (NumberFormatException e) {
                 return new Response("Not a valid Id", Status.BAD_REQUEST);
             }
-            
+
             DataRepository storage = DataRepository.getInstance();
             Doctor d = storage.findDoctorById(longId);
-            
-            if(d == null){
+
+            if (d == null) {
                 return new Response("Doctor not found", Status.BAD_REQUEST);
             }
-            
+
             HashMap<String, Object> username = new HashMap<>();
             String stringUsername = d.getUsername();
             username.put(id, stringUsername);
-            
+
             return new Response("Returned doctor's username", Status.OK, username);
-            
-            
-            
-        }catch(Exception e){
+
+        } catch (Exception e) {
             return new Response("Unexpected error", Status.INTERNAL_SERVER_ERROR);
         }
     }
-    
-    public static Response returnPatientUsernameToChangeView(String id){
-        
-        try{
+
+    public static Response returnPatientUsernameToChangeView(String id) {
+
+        try {
             long longId;
             try {
-                longId= Long.parseLong(id);
-                
+                longId = Long.parseLong(id);
+
             } catch (NumberFormatException e) {
                 return new Response("Not a valid Id", Status.BAD_REQUEST);
             }
-            
+
             DataRepository storage = DataRepository.getInstance();
             Patient p = storage.findPatientById(longId);
-            if(p == null){
+            if (p == null) {
                 return new Response("Patient not found", Status.BAD_REQUEST);
             }
-            
+
             HashMap<String, Object> username = new HashMap<>();
             String stringUsername = p.getUsername();
             username.put(id, stringUsername);
-            
+
             return new Response("Returned doctor's username", Status.OK, username);
-            
-            
-            
-        }catch(Exception e){
+
+        } catch (Exception e) {
             return new Response("Unexpected error", Status.INTERNAL_SERVER_ERROR);
         }
     }
 }
-
